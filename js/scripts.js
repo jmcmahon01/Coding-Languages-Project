7b824f64cdd195f3bfe671cb1dcd626985640509
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-11-04 10:16:34 -0700

    replace original event handlers with event listeners and add reset button which clears answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
-window.onload = function () {
+window.addEventListener("load", function () {
   let form = document.querySelector("form");
-  form.onsubmit = function (event) {
+  let resetBtn = document.querySelector("button#reset");
+  let age = document.querySelector("div#age");
+
+  form.addEventListener("submit", function (event) {
+
     const userAge = document.getElementById("userAge").value;
     const hairColor = document.getElementById("hairColor").value;
     const height = document.getElementById("height").value;
@@ -9,10 +13,40 @@ window.onload = function () {
     document.querySelector("span#hairColor").innerText = "It doesn't matter whether or not you even have hair, let alone what color it is. Anyone can learn to code!";
     document.querySelector("span#height").innerText = "Don't listen to Randy Newman, short people both have a reason to live and they can also learn to code!"
 
-
-    document.querySelector("div#age").removeAttribute("class");
+    age.removeAttribute("class")
 
     event.preventDefault();
-  };
-};
+  });
+
+  form.addEventListener("submit", function () {
+    resetBtn.removeAttribute("class");
+  });
+
+  resetBtn.addEventListener("click", function () {
+    age.setAttribute("class", "hidden");
+    document.getElementById("userAge").value = null;
+    document.getElementById("hairColor").value = null;
+    document.getElementById("height").value = null;
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

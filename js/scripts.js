2a3fd36c9a8f34505c3658d33b90a18ffa9cf33c
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-11-05 12:06:32 -0800

    re-structure JS code to have the correct scope for the reset button to appear and function correctly. Deleted all other input questions and will add them in later

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,41 @@
 window.addEventListener("load", function () {
   let form = document.querySelector("form");
   let resetBtn = document.querySelector("button#reset");
-
-
-
-
-
+  let javaScript = document.getElementById("language1");
+  let python = document.getElementById("language2");
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-    let javaScript = document.getElementById("language1");
     javaScript.setAttribute("class", "hidden");
-    let python = document.getElementById("language2");
     python.setAttribute("class", "hidden");
 
     const userAge = parseInt(document.getElementById("userAge").value);
-    event.preventDefault();
 
     if (userAge >= 18) {
       javaScript.removeAttribute("class");
     } else {
       python.removeAttribute("class");
     }
+
+    resetBtn.removeAttribute("class");
+  });
+
+  resetBtn.addEventListener("click", function () {
+    javaScript.setAttribute("class", "hidden");
+    python.setAttribute("class", "hidden");
+    document.getElementById("userAge").value = "";
+    resetBtn.setAttribute("class", "hidden");
   });
 });
 
-form.addEventListener("submit", function () {
-  resetBtn.removeAttribute("class");
-});
 
-resetBtn.addEventListener("click", function () {
-  javaScript.setAttribute("class", "hidden");
-  python.setAttribute("class", "hidden")
-  document.getElementById("userAge").value = null;
 
-});
+
+
+
+
+
+
+
 
 

4529804416491354aaa4cf7f470229b25c3dde57
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-11-05 11:49:50 -0800

    add branching statement for user age input

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,39 @@
 window.addEventListener("load", function () {
   let form = document.querySelector("form");
   let resetBtn = document.querySelector("button#reset");
-  let physicalCharacteristics = document.querySelector("div#physicalCharacteristics");
+
+
+
+
 
 
   form.addEventListener("submit", function (event) {
-    const userAge = document.getElementById("userAge").value;
-    const hairColor = document.getElementById("hairColor").value;
-    const height = document.getElementById("height").value;
-    const favColor = document.getElementById("favColor").value;
-    const favAnimal = document.getElementById("favAnimal").value;
-
-    document.querySelector("#age").innerText = userAge;
-    document.querySelector("#hair").innerText = hairColor;
-    document.querySelector("#shortTall").innerText = height;
-    document.querySelector("#color").innerText = favColor;
-    document.querySelector("#animal").innerText = favAnimal;
-
-    physicalCharacteristics.removeAttribute("class");
-
     event.preventDefault();
-  });
+    let javaScript = document.getElementById("language1");
+    javaScript.setAttribute("class", "hidden");
+    let python = document.getElementById("language2");
+    python.setAttribute("class", "hidden");
 
-  form.addEventListener("submit", function () {
-    resetBtn.removeAttribute("class");
-  });
+    const userAge = parseInt(document.getElementById("userAge").value);
+    event.preventDefault();
 
-  resetBtn.addEventListener("click", function () {
-    physicalCharacteristics.setAttribute("class", "hidden");
-    document.getElementById("userAge").value = null;
-    document.getElementById("hairColor").value = null;
-    document.getElementById("height").value = null;
-    document.getElementById("favColor").value = null;
-    document.getElementById("favAnimal").value = null;
+    if (userAge >= 18) {
+      javaScript.removeAttribute("class");
+    } else {
+      python.removeAttribute("class");
+    }
   });
 });
+
+form.addEventListener("submit", function () {
+  resetBtn.removeAttribute("class");
+});
+
+resetBtn.addEventListener("click", function () {
+  javaScript.setAttribute("class", "hidden");
+  python.setAttribute("class", "hidden")
+  document.getElementById("userAge").value = null;
+
+});
+
 

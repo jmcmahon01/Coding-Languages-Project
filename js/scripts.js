90812b513d1e6866188a28cd1f99a1242ae3fe44
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-11-05 12:22:57 -0800

    add an else if statement to provide 3 possible branch outcomes

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,20 @@ window.addEventListener("load", function () {
   let resetBtn = document.querySelector("button#reset");
   let javaScript = document.getElementById("language1");
   let python = document.getElementById("language2");
+  let cSharp = document.getElementById("language3");
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
     javaScript.setAttribute("class", "hidden");
+    cSharp.setAttribute("class", "hidden");
     python.setAttribute("class", "hidden");
 
     const userAge = parseInt(document.getElementById("userAge").value);
 
-    if (userAge >= 18) {
+    if (userAge > 18) {
       javaScript.removeAttribute("class");
+    } else if (userAge === 18) {
+      cSharp.removeAttribute("class");
     } else {
       python.removeAttribute("class");
     }
@@ -22,6 +26,7 @@ window.addEventListener("load", function () {
 
   resetBtn.addEventListener("click", function () {
     javaScript.setAttribute("class", "hidden");
+    cSharp.setAttribute("class", "hidden");
     python.setAttribute("class", "hidden");
     document.getElementById("userAge").value = "";
     resetBtn.setAttribute("class", "hidden");

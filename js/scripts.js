648ab310d6caa7a821e5b76db466de5457cf9387
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-11-05 10:38:56 -0800

    add two more input options to form, display all input values upon submission, and clear all input values on reset

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,14 @@ window.addEventListener("load", function () {
     const userAge = document.getElementById("userAge").value;
     const hairColor = document.getElementById("hairColor").value;
     const height = document.getElementById("height").value;
+    const favColor = document.getElementById("favColor").value;
+    const favAnimal = document.getElementById("favAnimal").value;
 
     document.querySelector("#age").innerText = userAge;
     document.querySelector("#hair").innerText = hairColor;
     document.querySelector("#shortTall").innerText = height;
+    document.querySelector("#color").innerText = favColor;
+    document.querySelector("#animal").innerText = favAnimal;
 
     physicalCharacteristics.removeAttribute("class");
 
@@ -27,6 +31,8 @@ window.addEventListener("load", function () {
     document.getElementById("userAge").value = null;
     document.getElementById("hairColor").value = null;
     document.getElementById("height").value = null;
+    document.getElementById("favColor").value = null;
+    document.getElementById("favAnimal").value = null;
   });
 });
 
